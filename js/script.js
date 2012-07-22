bd9e9b59f29ec0ca24ced4d752fa36bbb7ea6ddb
Eric_WVGG <eric@whiskyvangoghgo.com>
2012-07-21 22:07:11 -0400

    variable juggling

--- js/script.js
+++ js/script.js
@@ -3,31 +3,36 @@
   $.fn.blurryTransition = function(arguments) {
 
     var properties = $.extend({
-      'frame' : null, /* generated frame */
-      'canvas' : null,
-      'cycle' : null,
-      'i' : 0,
       'interval' : 12000,
       'css_transition_speed' : '0.7s',
       'blur_mode' : 'boxBlurImage',
-      'z_index' : 100,
       'width' : null,
-      'height' : null,
-      'offset' : {}
+      'height' : null
     }, arguments);
 
     return this.each(function() {
 
       var $image_index = 0,
+        cycle_index = 0,
+        z_index = 100,
         $list = '',
         $canvas = '',
-        first_img;
+        first_img = null,
+        offset = {},
+        cycle = null;
       
       var methods = {
         init : function(options) {
           return $(this).each(function() {
             $list = $(this);
             if (options) $.extend(properties, options);
+            // if width and height not specified, attempt to get dimensions
+              if( properties.width == null) properties.width = $list.width();
+              if( properties.height == null) properties.height = $list.height();
+              $list.css({
+                width : properties.width,
+                height : properties.height,
+              });
             // create list wrapper
               $list.wrap('<div class="blurryTransition_wrapper"/>');
               $('.blurryTransition_wrapper').css({
@@ -51,17 +56,17 @@
                       'width':adjusted_size.x,
                       'height':'auto',
                     });
-                    properties.offset = crop_image_to_screen(adjusted_size);
-                    var side = properties.offset.side;
+                    offset = crop_image_to_screen(adjusted_size, $list);
+                    var side = offset.side;
                     $(n).css({ 
                       'margin' : 0,
                       'padding' : 0,
                       'position' : 'absolute',
                       'top' : '0px',
-                      'z-index' : properties.z_index,
-                    }).css(properties.offset.side, properties.offset.amount);
+                      'z-index' : z_index,
+                    }).css(offset.side, offset.amount);
                     $(n).addClass('blurryTransition_frame').find('img').attr('id', 'blurryTransition_canvas_'+i);
-                    properties.z_index++;
+                    z_index++;
                   });
                 // set up first canvas
                   $canvas = $('<canvas/>');
@@ -76,17 +81,17 @@
                     '-webkit-transition-property' : 'opacity',
                     '-webkit-transition-duration' : properties.css_transition_speed,
                     '-webkit-transition-timing-function' : 'ease-out',
-                    'z-index' : properties.z_index,
+                    'z-index' : z_index,
                     'position' : 'absolute',
                     'opacity' : 0,
-                  }).css(properties.offset.side, properties.offset.amount);
+                  }).css(offset.side, offset.amount);
                   $list.before($canvas);
                 // set up second canvas
                   $canvas_2 = $canvas.clone();
                   $canvas_2.attr({
                     id : 'blurryTransition_canvas_c2',
                   }).css({
-                    'z-index' : properties.z_index + 1,
+                    'z-index' : z_index + 1,
                   });
                   $list.before($canvas_2);
                 
@@ -98,64 +103,48 @@
           // done setting up canvases
         },
         bind_window_resize : function() {
-
         	$(window).resize(function(){
-              var $img = $list.find('li:first-child img');
-              first_img = new Image();
-              first_img.onload = function() {
-                // calculate image cover sizes
-                  var adjusted_size = size_image_to_screen(first_img.width,first_img.height,$list);
-                // set up slides
-                  $list.find('li').each(function(i,n) {
-                    $(n).find('img').css({
-                      'width':adjusted_size.x,
-                      'height':'auto',
-                    });
-                    properties.offset = crop_image_to_screen(adjusted_size);
-                    $(n).css({ 
-                      'margin' : 0,
-                      'padding' : 0,
-                      'position' : 'absolute',
-                      'top' : '0px',
-                      'z-index' : properties.z_index
-                    }).css(properties.offset.side, properties.offset.amount);
-                    $(n).addClass('blurryTransition_frame').find('img').attr('id', 'blurryTransition_canvas_'+i);
-                    properties.z_index++;
-                  });
-                // set up first canvas
-                  $('.blurryTransition_canvas').css({
-                    'width' : adjusted_size.x + 'px !important',
-                    'height' : adjusted_size.y + 'px !important',
-                  }).css(properties.offset.side, properties.offset.amount);
-                
-              }
-              first_img.src = $img.attr('src');
+            // calculate image cover sizes
+              var adjusted_size = size_image_to_screen(first_img.width,first_img.height,$list);
+              offset = crop_image_to_screen(adjusted_size, $list);
+            // adjust slides
+              $list.find('li').each(function(i,n) {
+                $(n).find('img').css({
+                  'width':adjusted_size.x,
+                  'height':'auto',
+                });
+                $(n).css(offset.side, offset.amount);
+              });
+            // adjust canvas
+              $('.blurryTransition_canvas').css({
+                'width' : adjusted_size.x + 'px !important',
+                'height' : adjusted_size.y + 'px !important',
+              }).css(offset.side, offset.amount);
         	});
-
         },
         startCycle : function() {
-          properties.cycle = setInterval(function() {
+          cycle = setInterval(function() {
             methods.shiftImage();
           }, properties.interval);
         },
         stopCycle : function() {
-          clearInterval(properties.cycle);
+          clearInterval(cycle);
         },
         shiftImage : function() {
-          next_i = properties.i+1;
+          next_i = cycle_index+1;
           if(next_i >= $list.find('li').length) next_i = 0;
           methods.shiftImageTo(next_i);
         },
         shiftImageTo : function(destination_frame) {
           // note: confusing? i+1 because CSS indexes from 1, not 0
     			// prepare blurred image canvases
-      			boxBlurImage( 'blurryTransition_canvas_'+properties.i, 'blurryTransition_canvas_c1', 40, false, 1 );
+      			boxBlurImage( 'blurryTransition_canvas_'+cycle_index, 'blurryTransition_canvas_c1', 40, false, 1 );
             if(destination_frame > $list.find('li').length) b = 0;
       			boxBlurImage( 'blurryTransition_canvas_'+destination_frame, 'blurryTransition_canvas_c2', 40, false, 1 );
           // "blur" the current image (fade in its blurred canvas)
           $('#blurryTransition_canvas_c1').removeClass('fast').bind('webkitTransitionEnd oTransitionEnd transitionend MSTransitionEnd transitionend MSTransitionEnd', function() {
             // hide the current image
-            $list.find('li:nth-child('+(properties.i+1)+')').addClass('hidden');
+            $list.find('li:nth-child('+(cycle_index+1)+')').addClass('hidden');
             // simultaneously fade out that blurred canvas…
             $('#blurryTransition_canvas_c1').unbind().css('opacity', 0);
             // … and fade in the new blurred image canvas
@@ -165,7 +154,7 @@
               $list.find('li:nth-child('+(destination_frame+1)+')').removeClass('hidden');
     					// and finally fade out the new blurred image canvas
     					$('#blurryTransition_canvas_c2').addClass('slow').css('opacity', 0);
-              properties.i = destination_frame;
+              cycle_index = destination_frame;
             }).css('opacity', 1);
           }).css('opacity', 1);
         }
@@ -180,8 +169,8 @@
 
 $(window).load(function() {
   $('#images').blurryTransition({
-    'width' : '100%',
-    'height' : '100%',
+    width : '100%',
+    height : '100%'
   });
 });
 
@@ -207,16 +196,16 @@ function size_image_to_screen(x,y,parent) {
 } /* end function size_image_to_screen() */
 
 
-function crop_image_to_screen(dimensions) {
+function crop_image_to_screen(dimensions, $parent) {
 	var crop = new Object()
 	crop.side = '';
 	crop.amount = 0;
-	if( (dimensions.x-$(window).width()) > (dimensions.y-$(window).height()) ) {
+	if( (dimensions.x-$(window).width()) > (dimensions.y-$parent.height()) ) {
 		crop.side = 'margin-left';
-		crop.amount = -1 * (dimensions.x-$(window).width())/2 + 'px';
+		crop.amount = -1 * (dimensions.x-$parent.width())/2 + 'px';
 	} else {
 		crop.side = 'margin-top';
-		crop.amount = -1 * (dimensions.y-$(window).height())/2 + 'px';
+		crop.amount = -1 * (dimensions.y-$parent.height())/2 + 'px';
 	}
 	return crop;
 } /* end function crop_image_to_screen() */
